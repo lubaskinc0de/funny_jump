226870f9ef65f385ad8485dc70ed8952543dce4c
lubaskin <lubaskincorporation@gmail.com>
2025-02-01 23:07:01 +0300

    fix jumping

--- src/funny_jump/game/platform_manager.py
+++ src/funny_jump/game/platform_manager.py
@@ -147,7 +147,10 @@ class PlatformManager:
 
         for platform_sprite in self.platforms:
             if self.player_sprite.rect.centery <= self.platform_spawn_height:
-                offset = (self.platform_spawn_height - self.player_sprite.rect.centery) * delta
+                if (self.player_sprite.player.on_ground is True) or self.player_sprite.player.velocity.y > 0:
+                    break
+
+                offset = (self.platform_spawn_height - self.player_sprite.rect.centery) * delta * 3
                 platform_sprite.set_position(platform_sprite.rect.centerx, platform_sprite.rect.centery + offset)
 
                 if self.get_highest_platform().rect.centery > MAX_PLATFORM_HEIGHT:
